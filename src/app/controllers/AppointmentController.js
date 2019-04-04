d0f9fcb546d1713f33fa7c37ad9195d97ba432c6
Michael Pereira <mikepss@icloud.com>
2019-04-04 08:59:59 -0300

    Import

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,27 @@
+const { User, Appointment } = require('../models')
+
+class AppointmentController {
+  async create (req, res) {
+    const provider = await User.findByPk(req.params.provider)
+
+    return res.render('appointments/create', { provider })
+  }
+
+  async store (req, res) {
+    const { id } = req.session.user
+    const { provider } = req.params
+    const { date } = req.body
+
+    console.log(id, provider, date)
+
+    await Appointment.create({
+      user_id: id,
+      provider_id: 1,
+      date
+    })
+
+    return res.redirect('/app/dashboard')
+  }
+}
+
+module.exports = new AppointmentController()
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'robb0457.publiccloud.com.br',
+  username: 'mikep_gobarber',
+  password: 'Pit82199#',
+  database: 'mikepss_gobarber',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
